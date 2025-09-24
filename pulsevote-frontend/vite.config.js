bd6882738a2fbde98c00617c5974da150bc93a52
ST10405022 <ST10405022@vcconnect.edu.za>
2025-09-24 16:42:47 +0200

    Add SSL certificate files to .gitignore and configure Vite server for HTTPS

--- pulsevote-frontend/vite.config.js
+++ pulsevote-frontend/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+  plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('ssl/key.pem'),
+      cert: fs.readFileSync('ssl/cert.pem'),
+    }
+  }
+});
